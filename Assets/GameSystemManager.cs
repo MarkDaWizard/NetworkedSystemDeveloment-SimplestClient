143fa6bd5b384be65abedc4d09c263c104284641
Mark <wushu4@yahoo.com.vn>
2021-11-16 22:33:00 -0500

    Initial commit

---
+++ Assets/GameSystemManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameSystemManager : MonoBehaviour
+{
+    
+    public GameObject networkedClient;
+    
+    //static GameObject instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
+
+static public class GameStates
+{
+    public const int LoginMenu = 1;
+    public const int MainMenu = 2;
+    public const int WaitingInQueue = 3;
+    public const int TicTacToe = 4;
+    public const int WaitingForPlayer = 5;
+    public const int Running = 6;
+    public const int Observer = 7;
+}
---
+++ Assets/NetworkedClient.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkedClient : MonoBehaviour
+{
+
+    int connectionID;
+    int maxConnections = 1000;
+    int reliableChannelID;
+    int unreliableChannelID;
+    int hostID;
+    int socketPort = 5491;//5492
+    byte error;
+    bool isConnected = false;
+    int ourClientID;
+    GameObject gameSystemManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] allobjects = FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allobjects)
+        {
+            if (go.GetComponent<GameSystemManager>()!=null)
+            {
+                gameSystemManager = go;
+            }
+        }
+            Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        UpdateNetworkConnection();
+    }
+
+    private void UpdateNetworkConnection()
+    {
+        if (isConnected)
+        {
+            int recHostID;
+            int recConnectionID;
+            int recChannelID;
+            byte[] recBuffer = new byte[1024];
+            int bufferSize = 1024;
+            int dataSize;
+            NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostID, out recConnectionID, out recChannelID, recBuffer, bufferSize, out dataSize, out error);
+
+            switch (recNetworkEvent)
+            {
+                case NetworkEventType.ConnectEvent:
+                    Debug.Log("connected.  " + recConnectionID);
+                    ourClientID = recConnectionID;
+                    break;
+                case NetworkEventType.DataEvent:
+                    string msg = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                    ProcessRecievedMsg(msg, recConnectionID);
+                    Debug.Log("got msg = " + msg);
+                    break;
+                case NetworkEventType.DisconnectEvent:
+                    isConnected = false;
+                    Debug.Log("disconnected.  " + recConnectionID);
+                    break;
+            }
+        }
+    }
+    
+    private void Connect()
+    {
+
+        if (!isConnected)
+        {
+            Debug.Log("Attempting to create connection");
+
+            NetworkTransport.Init();
+
+            ConnectionConfig config = new ConnectionConfig();
+            reliableChannelID = config.AddChannel(QosType.Reliable);
+            unreliableChannelID = config.AddChannel(QosType.Unreliable);
+            HostTopology topology = new HostTopology(config, maxConnections);
+            hostID = NetworkTransport.AddHost(topology, 0);
+            Debug.Log("Socket open.  Host ID = " + hostID);
+
+            connectionID = NetworkTransport.Connect(hostID, "192.168.5.145", socketPort, 0, out error); // server is local on network
+
+            if (error == 0)
+            {
+                isConnected = true;
+
+                Debug.Log("Connected, id = " + connectionID);
+
+            }
+        }
+    }
+    
+    public void Disconnect()
+    {
+        NetworkTransport.Disconnect(hostID, connectionID, out error);
+    }
+    
+    public void SendMessageToHost(string msg)
+    {
+        byte[] buffer = Encoding.Unicode.GetBytes(msg);
+        NetworkTransport.Send(hostID, connectionID, reliableChannelID, buffer, msg.Length * sizeof(char), out error);
+    }
+
+    private void ProcessRecievedMsg(string msg, int id)
+    {
+        Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+        //chk message 
+        string[] csv = msg.Split(',');
+        if (csv.Length > 0)
+        {
+            int signifier = int.Parse(csv[0]);
+            
+        }
+    }
+
+    public bool IsConnected()
+    {
+        return isConnected;
+    }
+
+
+}
+public static class ClientToServerSignifiers
+{
+    public const int CreateAccount = 1;
+    public const int Login = 2;
+    public const int JoinGammeRoomQueue = 3;
+    public const int PlayGame = 4;
+    public const int SendMsg = 5;
+    public const int SendPrefixMsg = 6;
+    public const int JoinAsObserver = 7;
+    public const int SendClientMsg = 8;
+    public const int ReplayMsg = 9;
+}
+public static class ServerToClientSignifiers
+{
+    public const int LoginComplete = 1;
+    public const int LoginFailed = 2;
+    public const int AccountCreationComplete = 3;
+    public const int AccountCreationFailed = 4;
+    public const int OpponentPlay = 5;
+    public const int GameStart = 6;
+    public const int ReceiveMsg = 7;
+    public const int someoneJoinedAsObserver = 8;
+    public const int JoinedPlay = 9;
+    public const int ReceiveCMsg = 10;
+    public const int ReplayMsg = 11;
+}
